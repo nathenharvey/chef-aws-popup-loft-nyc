54d5b98849f73b29e2e2a67bc427f777f803811c
Nathen Harvey <nharvey@chef.io>
2015-10-21 12:30:12 -0400

    write cross-platform unit tests with ChefSpec

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,10 +7,29 @@
 require 'spec_helper'
 
 describe 'apache::default' do
-  context 'When all attributes are default, on an unspecified platform' do
+  context 'When all attributes are default, on an ubuntu platform' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(:platform => 'ubuntu', :version => '14.04')
       runner.converge(described_recipe)
+    end
+
+    it 'installs apache' do
+      expect(chef_run).to install_package 'apache2'
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+  end
+
+  context 'When all attributes are default, on an centos platform' do
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new(:platform => 'centos', :version => '6.4')
+      runner.converge(described_recipe)
+    end
+
+    it 'installs apache' do
+      expect(chef_run).to install_package 'httpd'
     end
 
     it 'converges successfully' do

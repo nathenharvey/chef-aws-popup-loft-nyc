3f52bc7a061fd856a39244f8943274ffa67aa6e0
Nathen Harvey <nharvey@chef.io>
2015-10-21 11:02:27 -0400

    Write the serverspec tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,23 @@ require 'spec_helper'
 describe 'apache::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  it 'is awesome' do
+    expect(true).to be true
+  end
+
+  it 'is running' do
+    expect(service 'apache2').to be_running
+  end
+
+  it 'is installed' do
+    expect(package 'apache2').to be_installed
+  end
+
+  it 'is listening on the desired port' do
+    expect(port 80).to be_listening
+  end
+
+  it 'responds to an http request' do
+    expect(command('wget http://localhost').exit_status).to eq 0
   end
 end
